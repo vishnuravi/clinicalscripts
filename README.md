22a0b0c60ba5692367ab5a90e8d96aba7103ece8
Vishnu Ravi <vishnu@vishnu.io>
2015-03-19 00:06:38 -0400

    improve input validationfor anc

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # clinicalscripts
 A collection of python scripts used to do quick clinical calculations on the command line.
+
 # Scripts
 - ag.py - Calculates anion gap, delta gap and delta ratio
 - anc.py - Calculates absolute neutrophil count
--- anc.py
+++ anc.py
@@ -14,15 +14,35 @@ def get_float(min, prompt):
 			print "\n"
 			exit(0)
 		else:
-			return value
+			if value < 0:
+				print "This value cannot be less than 0."
+			else:
+				return value
+
+def get_percent(prompt):
+	while True:
+		try:
+			value = float(raw_input(prompt))
+		except ValueError:
+			print "Please enter a number."
+		except KeyboardInterrupt:
+			print "\n"
+			exit(0)
+		else:
+			if value < 0:
+				print "This value cannot be less than 0."
+			elif value > 100:
+				print "This value cannot be more than 100."
+			else:
+				return value
 
 
 def main():
 	print "\nAbsolute Neutrophil Count"
 	print "-----------------------------"
 	wbc = get_float(0, "WBC Count (in 1000s): ")
-	pmn = get_float(0, "Percent Segmented Neutrophils (%): ")
-	bands = get_float(0, "Percent Bands (%): ")
+	pmn = get_percent("Percent Segmented Neutrophils (%): ")
+	bands = get_percent("Percent Bands (%): ")
 	anc = calc_anc(wbc, pmn, bands)
 	print "\nAbsolute Neutrophil Count is " + str(anc) + " x 10^3/uL\n"
 
