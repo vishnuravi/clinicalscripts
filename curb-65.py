c56ef8e08170df03b99720a7bda24d228a1d8f5d
Vishnu Ravi <vishnu@vishnu.io>
2015-03-25 23:22:47 -0400

    add interpretation to CURB-65

--- curb-65.py
+++ curb-65.py
@@ -28,20 +28,34 @@ def ask_yes_no(question, default="no"):
 			return valid[user_choice]
 		else:
 			sys.stdout.write("Please respond either 'yes' or 'no'.")
-
+	
 def score_to_mortality(score):
 	"""Returns the 30 day mortality rate for the given CURB score"""
 	return [0.6, 2.7, 6.8, 14.0, 27.8, 27.8][score]
 
+def interpret_score(score):
+	"""Returns interpretation of the CURB-65 score as a string"""
+	if 0 <= score <= 1:
+		interpretation = "Consider treating as an outpatient."
+	elif 2 <= score <=3:
+		interpretation = "Consider inpatient treatment or outpatient with close followup."
+	elif 4 <= score <=5:
+		interpretation = "Requires hospitalization, consider admitting to ICU."
+	else:
+		interpretation = "Invalid score: Must be between 0 and 5."
+
+	return interpretation
 
 def main():
+    """ Predicts mortality in community-acquired pneumonia using CURB-65 criteria """
+
 	print "\nCURB-65 Severity Score", "\n----------------------"
 	
 	criteria = ['Confusion', 'BUN > 19 mg/dL', 'Resp. Rate >= 30', 'Systolic BP < 90 mmHg or Diastolic BP <= 60 mmHg', 'Age >= 65']
 	
 	score = sum(map(ask_yes_no, criteria))
 
-	print "\nCURB-65 Score: {}, 30-day mortality rate: {}%".format(score, score_to_mortality(score))
+	print "\nCURB-65 Score: {} \n30-day mortality rate: {}% \nInterpretation: {} \n".format(score, score_to_mortality(score), interpret_score(score))
 
 
 
