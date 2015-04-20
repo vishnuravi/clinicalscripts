8b2f7692b9a4a3b050103995363612d9512c0929
Vishnu Ravi <vishnu@vishnu.io>
2015-04-20 11:12:41 -0700

    update from 34000 feet

---
+++ sirs.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+""" SIRS, Sepsis, Septic Shock Calculator 
+
+SIRS Criteria (>2):
+1. Body temperature > 100.4F (38C) or < 96.8F (36C)
+2. Heart Rate > 90 BPM
+3. Respiratory rate > 20 breaths per minute or PaCO2 < 32 mm Hg
+4. WBC count < 4,000 cells/mm^3 or > 12,000 cells/mm^3
+
+Sepsis Criteria: SIRS + Source of Infection
+
+Severe Sepsis: Sepsis + Organ Dysfunction
+
+Septic Shock: Severe Sepsis + Hypotension, despite adequate fluid resuscitation
+
+"""
+
